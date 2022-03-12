9d12e1d0dbcbebf57407aab08b3de1c4374f4966
d-plaindoux <d.plaindoux@free.fr>
2022-03-12 13:57:07 +0100

    Format

--- ocaml/test/nethra/render/tests.ml
+++ ocaml/test/nethra/render/tests.ml
@@ -1,1 +1,10 @@
-let () = Alcotest.(run "Test" [ T01_basic.cases; T02_function.cases; T03_pair.cases; T04_sum.cases; T05_mu.cases ])
+let () =
+  Alcotest.(
+    run "Test"
+      [
+        T01_basic.cases
+      ; T02_function.cases
+      ; T03_pair.cases
+      ; T04_sum.cases
+      ; T05_mu.cases
+      ])
