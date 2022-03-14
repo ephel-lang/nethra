3f8660559b3500e646a5c07f5bf780f216d8507f
d-plaindoux <d.plaindoux@free.fr>
2022-03-14 09:02:00 +0100

    Check/Infer in progress

--- ocaml/lib/nethra/system/checker.mli
+++ ocaml/lib/nethra/system/checker.mli
@@ -1,9 +1,1 @@
-module Impl : functor (Infer : Specs.Infer) -> sig
-  include module type of Goal
-
-  val ( <?:> ) :
-       'a Nethra_ast.Term.t
-    -> 'a Nethra_ast.Term.t
-    -> 'a Nethra_ast.Bindings.t
-    -> 'a Nethra_ast.Proof.t
-end
+module Impl : functor (Infer : Specs.Infer) -> Specs.Checker
--- ocaml/lib/nethra/system/infer.mli
+++ ocaml/lib/nethra/system/infer.mli
@@ -1,9 +1,1 @@
-module Impl : functor (Checker : Specs.Checker) -> sig
-  include module type of Goal
-
-  val ( <?:> ) :
-       'a Nethra_ast.Term.t
-    -> unit
-    -> 'a Nethra_ast.Bindings.t
-    -> 'a Nethra_ast.Term.t option * 'a Nethra_ast.Proof.t
-end
+module Impl : functor (Checker : Specs.Checker) -> Specs.Infer
