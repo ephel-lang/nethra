828b00519533c9c381bad82ab2e187d445c0cabb
d-plaindoux <d.plaindoux@free.fr>
2022-03-15 08:43:31 +0100

    Term reduction revision using Preface

--- ocaml/lib/nethra/system/reduce.ml
+++ ocaml/lib/nethra/system/reduce.ml
@@ -2,24 +2,21 @@ open Nethra_ast.Ast.Term.Builders
 open Nethra_ast.Ast.Term.Catamorphism
 open Nethra_ast.Ast.Bindings.Access
 open Substitution
+open Preface.Option.Monad
 
 let reduce_id reduce bindings (name, _, _) =
-  Option.bind (get_definition bindings name) (reduce bindings)
+  get_definition bindings name >>= reduce bindings
 
 let reduce_apply reduce bindings (abstraction, argument, implicit, c) =
-  match reduce bindings abstraction with
-  | None -> None
-  | Some term ->
-    fold_opt
-      ~lambda:(fun (n, body, implicit', _) ->
-        if implicit = implicit'
-        then reduce bindings (substitute n argument body)
-        else if implicit'
-        then
-          reduce bindings
-            (apply ~c ~implicit (substitute n (hole n) body) argument)
-        else None )
-      term
+  reduce bindings abstraction
+  >>= fold_opt ~lambda:(fun (n, body, implicit', _) ->
+          if implicit = implicit'
+          then reduce bindings (substitute n argument body)
+          else if implicit'
+          then
+            reduce bindings
+              (apply ~c ~implicit (substitute n (hole n) body) argument)
+          else None )
 
 let reduce_fst reduce bindings (term, _c) =
   fold_opt ~pair:(fun (lhd, _, _) -> reduce bindings lhd) term
@@ -28,30 +25,25 @@ let reduce_snd reduce bindings (term, _c) =
   fold_opt ~pair:(fun (_, rhd, _) -> reduce bindings rhd) term
 
 let reduce_case reduce bindings (term, left, right, _) =
-  match reduce bindings term with
-  | None -> None
-  | Some term ->
-    fold_opt
-      ~inl:(fun (term, _) -> reduce bindings (apply left term))
-      ~inr:(fun (term, _) -> reduce bindings (apply right term))
-      term
+  reduce bindings term
+  >>= fold_opt
+        ~inl:(fun (term, _) -> reduce bindings (apply left term))
+        ~inr:(fun (term, _) -> reduce bindings (apply right term))
 
 let reduce_hole reduce bindings (_, reference, _) =
-  match !reference with None -> None | Some e -> reduce bindings e
+  !reference >>= reduce bindings
 
 let rec reduce_opt bindings term =
-  match
-    fold_opt
-      ~id:(reduce_id reduce_opt bindings)
-      ~apply:(reduce_apply reduce_opt bindings)
-      ~fst:(reduce_fst reduce_opt bindings)
-      ~snd:(reduce_snd reduce_opt bindings)
-      ~case:(reduce_case reduce_opt bindings)
-      ~hole:(reduce_hole reduce_opt bindings)
-      term
-  with
-  | None -> Some term
-  | some -> some
+  Option.fold ~none:(Some term)
+    ~some:(fun a -> Some a)
+    (fold_opt
+       ~id:(reduce_id reduce_opt bindings)
+       ~apply:(reduce_apply reduce_opt bindings)
+       ~fst:(reduce_fst reduce_opt bindings)
+       ~snd:(reduce_snd reduce_opt bindings)
+       ~case:(reduce_case reduce_opt bindings)
+       ~hole:(reduce_hole reduce_opt bindings)
+       term )
 
 let reduce bindings term =
-  match reduce_opt bindings term with None -> term | Some term -> term
+  Option.fold ~none:term ~some:(fun a -> a) (reduce_opt bindings term)
