6593c233c4ddb3abddf4e4499f124e601eeed3f4
d-plaindoux <d.plaindoux@free.fr>
2022-04-06 08:28:16 +0200

    Remove useless comment

--- ocaml/lib/nethra/syntax/parser/specs.mli
+++ ocaml/lib/nethra/syntax/parser/specs.mli
@@ -1,7 +1,6 @@
 module type Parsec = sig
   module Source : Nethra_syntax_source.Specs.Source
 
-  (* Profunctor -> Arrow? *)
   type 'a t = Source.t -> ('a, Source.t) Response.t
 
   val source : Source.Construct.c -> Source.t
