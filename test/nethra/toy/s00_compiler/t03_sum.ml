6f69b021b7393ba29b872fb8537315fe23159188
d-plaindoux <d.plaindoux@free.fr>
2022-05-17 06:37:06 +0200

    More tests

--- test/nethra/toy/s00_compiler/t03_sum.ml
+++ test/nethra/toy/s00_compiler/t03_sum.ml
@@ -58,10 +58,47 @@ let compile_recursive_sum () =
   and expected = Result.Ok true in
   Alcotest.(check (result bool string)) "recursive sum type" expected result
 
+let compile_recursive_sum_with_pseudo_constructors () =
+  let open Preface_stdlib.Result.Functor (struct
+    type t = string
+  end) in
+  let result =
+    Stage.run
+      {toy|
+        --- Preamble
+        sig int  : type
+        ------------
+        sig nilT  : type
+        sig Nil   : nilT
+        sig consT : type
+        sig Cons  : consT
+
+        sig list : (type) -> type
+        def list = (X).rec(l).(nilT | consT * X * l)
+
+        sig nil : {X:type} -> list X
+        def nil = fold inl Nil
+
+        sig cons : {X:type} -> X -> list X -> list X
+        def cons = (h).(l).fold inr (Cons,h,l)
+
+        sig test : list int
+        def test = cons 1 (nil {int})
+
+        sig append : {X:type} -> list X -> list X -> list X
+        def append = (l1).(l2).case (unfold l1) (_).l2 (c).(cons (fst (snd c)) (append (snd (snd c)) l2))
+        ------------
+      |toy}
+    <&> fun (_, l) -> check l
+  and expected = Result.Ok true in
+  Alcotest.(check (result bool string)) "recursive sum type" expected result
+
 let cases =
   let open Alcotest in
   ( "Sum Compiler"
   , [
       test_case "basic sum type" `Quick compile_basic_sum
     ; test_case "recursive sum type" `Quick compile_recursive_sum
+    ; test_case "recursive sum type with literals" `Quick
+        compile_recursive_sum_with_pseudo_constructors
     ] )
