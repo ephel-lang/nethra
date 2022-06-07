c310b60c49c3b11467595d5211c52e848d5a4457
d-plaindoux <d.plaindoux@free.fr>
2022-06-07 09:15:02 +0200

    Review context

--- lib/nethra/lang/ast/context.ml
+++ lib/nethra/lang/ast/context.ml
@@ -16,34 +16,51 @@ module Bindings = struct
 end
 
 module Hypothesis = struct
-  type 'a t = int ref * 'a Bindings.t * 'a Bindings.t
+  type 'a t = int ref * 'a Bindings.t * 'a Bindings.t * 'a Bindings.t
 
   (* State monad should be use here ... *)
 
   module Construct = struct
-    let create = (ref 0, Bindings.Construct.create, Bindings.Construct.create)
+    let create =
+      ( ref 0
+      , Bindings.Construct.create
+      , Bindings.Construct.create
+      , Bindings.Construct.create )
   end
 
   module Access = struct
-    let fresh_variable (r, gamma, omega) base =
+    let fresh_variable (r, gamma, omega, subst) base =
       let v = base ^ string_of_int !r in
       let () = r := !r + 1 in
-      (v, (r, gamma, omega))
+      (v, (r, gamma, omega, subst))
 
-    let get_signature (_, gamma, _) n = Bindings.Access.get_binding gamma n
+    (** Signatures *)
 
-    let add_signature (reference, gamma, delta) binding =
-      (reference, Bindings.Access.add_binding gamma binding, delta)
+    let signatures (_, gamma, _, _) = Bindings.Access.bindings gamma
+    let get_signature (_, gamma, _, _) n = Bindings.Access.get_binding gamma n
 
-    let add_signatures (reference, gamma, delta) signatures =
-      (reference, Bindings.Access.add_bindings gamma signatures, delta)
+    let add_signature (reference, gamma, delta, subst) binding =
+      (reference, Bindings.Access.add_binding gamma binding, delta, subst)
 
-    let signatures (_, l, _) = Bindings.Access.bindings l
-    let get_definition (_, _, delta) n = Bindings.Access.get_binding delta n
+    let add_signatures (reference, gamma, delta, subst) signatures =
+      (reference, Bindings.Access.add_bindings gamma signatures, delta, subst)
 
-    let add_definition (reference, gamma, delta) binding =
-      (reference, gamma, Bindings.Access.add_binding delta binding)
+    (** Definitions *)
 
-    let definitions (_, _, l) = Bindings.Access.bindings l
+    let definitions (_, _, delta, _) = Bindings.Access.bindings delta
+    let get_definition (_, _, delta, _) n = Bindings.Access.get_binding delta n
+
+    let add_definition (reference, gamma, delta, subst) binding =
+      (reference, gamma, Bindings.Access.add_binding delta binding, subst)
+
+    (** Substitutions *)
+
+    let substitutions (_, _, _, subst) = Bindings.Access.bindings subst
+
+    let get_substitution (_, _, _, subst) n =
+      Bindings.Access.get_binding subst n
+
+    let add_substitution (reference, gamma, delta, subst) binding =
+      (reference, gamma, delta, Bindings.Access.add_binding subst binding)
   end
 end
--- lib/nethra/lang/ast/context.mli
+++ lib/nethra/lang/ast/context.mli
@@ -22,12 +22,24 @@ module Hypothesis : sig
 
   module Access : sig
     val fresh_variable : 'a t -> string -> string * 'a t
+
+    (** Signatures *)
+
     val signatures : 'a t -> (string * 'a Term.t) list
     val get_signature : 'a t -> string -> 'a Term.t option
     val add_signature : 'a t -> string * 'a Term.t -> 'a t
     val add_signatures : 'a t -> (string * 'a Term.t) list -> 'a t
+
+    (** Definitions *)
+
+    val definitions : 'a t -> (string * 'a Term.t) list
     val get_definition : 'a t -> string -> 'a Term.t option
     val add_definition : 'a t -> string * 'a Term.t -> 'a t
-    val definitions : 'a t -> (string * 'a Term.t) list
+
+    (** Substitutions *)
+
+    val substitutions : 'a t -> (string * 'a Term.t) list
+    val get_substitution : 'a t -> string -> 'a Term.t option
+    val add_substitution : 'a t -> string * 'a Term.t -> 'a t
   end
 end
