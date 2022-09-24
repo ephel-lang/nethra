0412024f04735fd408a8329dcbbb4b3b6fdf5997
d-plaindoux <d.plaindoux@free.fr>
2022-09-24 06:57:28 +0200

    Minor refactoring

--- lib/nethra/lang/ast/hypothesis.ml
+++ lib/nethra/lang/ast/hypothesis.ml
@@ -1,3 +1,20 @@
+module Bindings = struct
+  type 'a t = (string * 'a Term.t) list
+
+  module Construct = struct
+    let create = []
+  end
+
+  module Access = struct
+    let get_binding bindings n =
+      Option.map snd (List.find_opt (fun c -> fst c = n) bindings)
+
+    let add_binding bindings binding = binding :: bindings
+    let add_bindings bindings more_bindings = more_bindings @ bindings
+    let bindings l = l
+  end
+end
+
 type 'a t = int * 'a Bindings.t * 'a Bindings.t * 'a Bindings.t
 
 (* State monad should be use here ... *)
