695ad9f27c868f0624c7b4d36dc4265c115e6572
d-plaindoux <d.plaindoux@free.fr>
2022-10-27 17:08:23 +0200

    Unused bindings removed

--- lib/nethra/lang/ast/hypothesis.ml
+++ lib/nethra/lang/ast/hypothesis.ml
@@ -15,39 +15,35 @@ module Bindings = struct
   end
 end
 
-type 'a t = int * 'a Bindings.t * 'a Bindings.t * 'a Bindings.t
+type 'a t = int * 'a Bindings.t * 'a Bindings.t
 
 (* State monad should be use here ... *)
 
 module Construct = struct
-  let create =
-    ( 0
-    , Bindings.Construct.create
-    , Bindings.Construct.create
-    , Bindings.Construct.create )
+  let create = (0, Bindings.Construct.create, Bindings.Construct.create)
 end
 
 module Access = struct
-  let fresh_variable (r, gamma, omega, subst) base =
+  let fresh_variable (r, gamma, omega) base =
     let v = base ^ string_of_int r in
-    (v, (r + 1, gamma, omega, subst))
+    (v, (r + 1, gamma, omega))
 
   (** Signatures *)
 
-  let signatures (_, gamma, _, _) = Bindings.Access.bindings gamma
-  let get_signature (_, gamma, _, _) n = Bindings.Access.get_binding gamma n
+  let signatures (_, gamma, _) = Bindings.Access.bindings gamma
+  let get_signature (_, gamma, _) n = Bindings.Access.get_binding gamma n
 
-  let add_signature (reference, gamma, delta, subst) binding =
-    (reference, Bindings.Access.add_binding gamma binding, delta, subst)
+  let add_signature (reference, gamma, delta) binding =
+    (reference, Bindings.Access.add_binding gamma binding, delta)
 
-  let add_signatures (reference, gamma, delta, subst) signatures =
-    (reference, Bindings.Access.add_bindings gamma signatures, delta, subst)
+  let add_signatures (reference, gamma, delta) signatures =
+    (reference, Bindings.Access.add_bindings gamma signatures, delta)
 
   (** Definitions *)
 
-  let definitions (_, _, delta, _) = Bindings.Access.bindings delta
-  let get_definition (_, _, delta, _) n = Bindings.Access.get_binding delta n
+  let definitions (_, _, delta) = Bindings.Access.bindings delta
+  let get_definition (_, _, delta) n = Bindings.Access.get_binding delta n
 
-  let add_definition (reference, gamma, delta, subst) binding =
-    (reference, gamma, Bindings.Access.add_binding delta binding, subst)
+  let add_definition (reference, gamma, delta) binding =
+    (reference, gamma, Bindings.Access.add_binding delta binding)
 end
