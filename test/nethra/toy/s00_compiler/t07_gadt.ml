41a66598fc6b2bdc0a52b59ee3bf0d82d79df1ac
d-plaindoux <d.plaindoux@free.fr>
2023-01-02 19:14:34 +0100

    Review test

--- test/nethra/toy/s00_compiler/t07_gadt.ml
+++ test/nethra/toy/s00_compiler/t07_gadt.ml
@@ -20,29 +20,31 @@ let compile_gadt () =
 
       sig int  : type
 
-     -{
+      val id_subst_right : {A B:type} -> (A * equals B A) -> B
+                         = (e).(subst fst e by snd e)
+      val id_subst_left  : {A B:type} -> (A * equals A B) -> B
+                         = {A B}.(e).(id_subst_right {A} {B} (fst e, subst refl by snd e))
+
+      -{
          data Expr A =
-         | Boolean  of Bool with A = Bool
-         | Integer  of int  with A = int
-     }-
+         | Boolean of Bool with A = Bool
+         | Integer of int  with A = int
+      }-
 
       sig Expr : (type) -> type
       val Expr = (A).((Bool * equals A Bool) | (int * equals A int))
 
-      sig boolean : {A:type} -> {_:equals A Bool} -> Bool -> Expr A
-      val boolean = {_ p}.(b).inl (b,p)
+      sig Boolean : {A:type} -> {_:equals A Bool} -> Bool -> Expr A
+      val Boolean = {_ p}.(b).inl (b,p)
 
-      sig number  : {A:type} -> {_:equals A int}  -> int  -> Expr A
-      val number  = {_ p}.(b).inr (b,p)
-
-      sig id_subst : {A B:type} -> (A * equals B A) -> B
-      val id_subst = (e).(subst fst e by snd e)
+      sig Integer : {A:type} -> {_:equals A int}  -> int  -> Expr A
+      val Integer = {_ p}.(b).inr (b,p)
 
       sig eval : {A:type} -> Expr A -> A
-      val eval = {A}.(e).case e (id_subst {Bool} {A}) (id_subst {int} {A})
+      val eval = {A}.(e).case e (id_subst_right {Bool} {A}) (id_subst_right {int} {A})
 
-      val res1 : int  = eval (number 1)
-      val res2 : Bool = eval (boolean true)
+      val res1 : int  = eval (Integer 1)
+      val res2 : Bool = eval (Boolean true)
       |toy}
     <&> fun (_, l) -> check l
   and expected = Result.Ok true in
